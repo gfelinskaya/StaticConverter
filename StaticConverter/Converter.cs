d49f9e2d551d7ca0453e44abfca5fd7e2ff7ade1
Halyna Felinska <g.felinskaya@gmail.com>
2025-04-28 12:58:44 +0300

    bbb

---
+++ StaticConverter/Converter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    static class Converter
+    {
+        private static double dollarRate;
+        private static double euroRate;
+
+        static Converter()
+        {
+            dollarRate = GetDollarRate();
+            euroRate = GetEuroRate();
+        }
+
+        private static double GetDollarRate()
+        {
+            return 42; // call database
+        }
+
+        private static double GetEuroRate()
+        {
+            return 45; // call database
+        }
+
+        public static double DallarRate 
+        {
+            get => dollarRate;
+            set => dollarRate = value;
+        }
+
+        public static double EuroRate
+        {
+            get => euroRate;
+            set => euroRate = value;
+        }
+
+
+        public static double ConvertEuroToHrn(double sum)
+        {
+            return euroRate * sum;
+        }
+
+        public static double ConvertDollarsToHrn(double sum)
+        {
+            return dollarRate * sum;
+        }
+
+        public static double ConvertHrnToEuro(double sum)
+        {
+            if (euroRate == 0)
+                return 0;   
+
+            return sum / euroRate;
+        }
+
+        public static double ConvertHrnToDollars(double sum)
+        {
+            if (dollarRate == 0)
+                return 0;
+
+            return sum / dollarRate;
+        }
+
+    }
+}
---
+++ StaticConverter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grns = Converter.ConvertDollarsToHrn(500);
+
+            Console.WriteLine($"Sum in hrn {grns}");
+
+
+            Converter.EuroRate = 47;
+            Converter.DallarRate = 42.5;
+
+
+            var grns2 = Converter.ConvertDollarsToHrn(500);
+            var dollars = Converter.ConvertHrnToDollars(10_000);
+
+            Console.WriteLine($"Dollars={dollars}");
+
+            Console.WriteLine($"Sum in hrn {grns2}");
+
+            Converter.EuroRate = 46;
+            Converter.DallarRate = 49;
+
+           var hrn3 = Converter.ConvertEuroToHrn(1000);
+           Console.WriteLine($"Hrn = {hrn3}");
+        }
+    }
+}
